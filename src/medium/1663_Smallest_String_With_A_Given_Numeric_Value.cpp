ccaf1a9dd4a15713da8ec0c41dab0178916519fc
HeapStackTree <baharainjaserua75@gmail.com>
2023-01-26 19:36:50 +0800

    1663. Smallest String With A Given Numeric Value

---
+++ src/medium/1663_Smallest_String_With_A_Given_Numeric_Value.cpp
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        string ans(n, 'a');
+        int i = n - 1, d = k - n;
+        for (; d > 25; d -= 25) {
+            ans[i--] = 'z';
+        }
+        ans[i] += d;
+        return ans;
+    }
+};
