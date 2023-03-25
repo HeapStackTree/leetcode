eff2f77e1fb45da82c9a4fe99d6a038e1f3c91ea
HeapStackTree <baharainjaserua75@gmail.com>
2023-03-25 22:37:59 +0800

    946. Validate Stack Sequences

---
+++ src/medium/946_Validate_Stack_Sequences.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> stk;
+        for (int i = 0, j = 0; i < pushed.size(); i++) {
+            stk.push(pushed[i]);
+            while (!stk.empty() && stk.top() == popped[j]) {
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
