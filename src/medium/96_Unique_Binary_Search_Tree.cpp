b7b6c8836d80cc67eeb696bd97bb93a3f4fbb402
HeapStackTree <baharainjaserua75@gmail.com>
2023-04-14 11:16:01 +0800

    96. Unique Binary Search Trees

---
+++ src/medium/96_Unique_Binary_Search_Tree.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+class Solution1 {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
+
+
+class Solution {
+public:
+    int numTrees(int n) {
+        long long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int)C;
+    }
+};
