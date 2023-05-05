0a5b31e2718a609575ed7c4c78b30b2360f9ffdd
HeapStackTree <baharainjaserua75@gmail.com>
2023-05-05 17:10:15 +0800

    1003. Check If Word Is Valid After Substitutions

---
+++ src/medium/1003_Check_If_Word_Is_Valid_After_Substitutions.cpp
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        string stk;
+        for (char c : s) {
+            stk.push_back(c);
+            if (stk.size() >= 3 && stk.substr(stk.size() - 3, 3) == "abc") {
+                stk.erase(stk.end() - 3, stk.end());
+            }
+        }
+        return stk.empty();
+    }
+};
