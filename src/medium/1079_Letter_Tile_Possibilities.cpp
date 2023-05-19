e880641a6ecd24c033fe60d1db1fa393b75b49ec
HeapStackTree <baharainjaserua75@gmail.com>
2023-05-19 16:12:13 +0800

    1079. Letter Tile Possibilities

---
+++ src/medium/1079_Letter_Tile_Possibilities.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <unordered_map>
+#include <set>
+using namespace std;
+class Solution {
+public:
+    int numTilePossibilities(string tiles) {
+        unordered_map<char, int> count;
+        set<char> tile;
+        int n = tiles.size();
+        for (char c: tiles) {
+            count[c]++;
+            tile.insert(c);
+        }
+        return dfs(count, tile, n) - 1;
+    }
+
+    int dfs(unordered_map<char, int>& count, set<char>& tile, int i) {
+        if (i == 0) {
+            return 1;
+        }
+        int res = 1;
+        for (char t: tile) {
+            if (count[t] > 0) {
+                count[t]--;
+                res += dfs(count, tile, i - 1);
+                count[t]++;
+            }
+        }
+        return res;
+    }
+};
