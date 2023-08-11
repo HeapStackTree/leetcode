efbc4a19400ec41ecda8f5cf7072883a8c5d381e
HeapStackTree <baharainjaserua75@gmail.com>
2023-08-11 19:04:42 +0800

    1572. Matrix Diagonal Sum

---
+++ src/easy/1572_Matrix_Diagonal_Sum.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res += mat[i][i] + mat[i][n - i -1];
+        }
+        if (n % 2 == 1) {
+            res -= mat[n / 2][n / 2];
+        }
+        return res;
+    }
+};
