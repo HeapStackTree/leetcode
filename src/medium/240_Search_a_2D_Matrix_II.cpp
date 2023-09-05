e2a6dc938341a6b31dc08545662fcbdd0a75c473
HeapStackTree <baharainjaserua75@gmail.com>
2023-09-05 14:20:43 +0800

    240. Search a 2D Matrix II

---
+++ src/medium/240_Search_a_2D_Matrix_II.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+using namespace std;
+
+class Solution1 {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for (const auto& row: matrix) {
+            auto it = lower_bound(row.begin(), row.end(), target);
+            if (it != row.end() && *it == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int x = 0, y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            if (matrix[x][y] < target) {
+                ++x;
+            } else {
+                --y;
+            }
+        }
+        return false;        
+    }
+};
