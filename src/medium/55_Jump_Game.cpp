8aa6869de848a12226a2f44b4d7a57b307dda6e6
HeapStackTree <baharainjaserua75@gmail.com>
2024-09-18 15:13:39 +0800

    55. Jump Game

---
+++ src/medium/55_Jump_Game.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
